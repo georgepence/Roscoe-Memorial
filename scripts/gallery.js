1cd62f02b2eb4b2456c9359f38ccd7b438668842
George Pence <penceg@oregonstate.edu>
2021-05-11 14:07:29 -0400

    Tuesday 2p commit

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,14 +1,14 @@
 
 window.addEventListener("load", function() {
-  document.getElementById("pose").classList.add("active");
+  document.getElementById("pose").classList.add("show");
   addScrollListeners();
 })
 
 function addScrollListeners() {
   document.getElementById("pals-thumb").addEventListener('click', function () {
-    console.log(document.getElementsByClassName("active"))
-    document.getElementsByClassName("active")[0].classList.remove("active");
-    document.getElementById("pals").classList.add("active");
+    console.log(document.getElementsByClassName("show"))
+    document.getElementsByClassName("show")[0].classList.remove("show");
+    document.getElementById("pals").classList.add("show");
   })
 }
 
