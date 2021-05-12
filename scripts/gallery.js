d1578320a4719b3ee82ac475b9b16fc1b6ad6b24
George Pence <penceg@oregonstate.edu>
2021-05-12 16:10:50 -0400

    Wednesday 4:15p commit

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -10,5 +10,35 @@ function addScrollListeners() {
     document.getElementsByClassName("show")[0].classList.remove("show");
     document.getElementById("pals").classList.add("show");
   })
+  document.getElementById("lex-thumb").addEventListener('click', function () {
+    console.log(document.getElementsByClassName("show"))
+    document.getElementsByClassName("show")[0].classList.remove("show");
+    document.getElementById("lex").classList.add("show");
+  })
+  document.getElementById("gate-thumb").addEventListener('click', function () {
+    console.log(document.getElementsByClassName("show"))
+    document.getElementsByClassName("show")[0].classList.remove("show");
+    document.getElementById("gate").classList.add("show");
+  })
+  document.getElementById("pals2-thumb").addEventListener('click', function () {
+    console.log(document.getElementsByClassName("show"))
+    document.getElementsByClassName("show")[0].classList.remove("show");
+    document.getElementById("pals2").classList.add("show");
+  })
+  document.getElementById("shower-thumb").addEventListener('click', function () {
+    console.log(document.getElementsByClassName("show"))
+    document.getElementsByClassName("show")[0].classList.remove("show");
+    document.getElementById("shower").classList.add("show");
+  })
+  document.getElementById("deer-thumb").addEventListener('click', function () {
+    console.log(document.getElementsByClassName("show"))
+    document.getElementsByClassName("show")[0].classList.remove("show");
+    document.getElementById("deer").classList.add("show");
+  })
+  document.getElementById("pose-thumb").addEventListener('click', function () {
+    console.log(document.getElementsByClassName("show"))
+    document.getElementsByClassName("show")[0].classList.remove("show");
+    document.getElementById("pose").classList.add("show");
+  })
 }
 
