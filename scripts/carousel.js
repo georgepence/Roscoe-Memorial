39aaec3f66c26979bcaed412ecf9179959b3a21a
George Pence <penceg@oregonstate.edu>
2021-05-16 09:34:45 -0400

    9:30 am Sunday Commit

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,62 +1,86 @@
 
 window.addEventListener("load", function() {
   let pics = document.getElementsByClassName("carousel-pics");
-  pic[pic.length - 1].style.opacity = "1.0";
+  // document.getElementById("c-pose").style.opacity = "1.0"
+  // pic = pics[pics.length - 1].id
+  // console.log(pic)
+  pics[pics.length - 1].style.opacity = "1.0";
+  console.log("here")
   runGallery(pics);
 })
 
 function runGallery(pics) {
-  while (true) {
-    for (let pic in pics) {
-      for (let opac = 0.0; opac <= 1.0; opac += 0.1) {
-        pic.style.opacity = toString(opac);
-        setTimeout(200)
-      }
-    }
+  console.log("here too")
+  let i = 0;
+  let numPics = pics.length;
+  let j = numPics - 1;
+  
+  while (i < numPics) {
+    setInterval(function () {(j, i) => {console.log(j, i)}}, 2000);
+    j = i;
+    i += 1;
   }
-  document.getElementById("pals-thumb").addEventListener('click', function () {
-    console.log(document.getElementsByClassName("show"))
-    document.getElementsByClassName("show")[0].classList.remove("show");
-    document.getElementById("pals").classList.add("show");
-  })
-  document.getElementById("lex-thumb").addEventListener('click', function () {
-    console.log(document.getElementsByClassName("show"))
-    document.getElementsByClassName("show")[0].classList.remove("show");
-    document.getElementById("lex").classList.add("show");
-  })
-  document.getElementById("cave-thumb").addEventListener('click', function () {
-    console.log(document.getElementsByClassName("show"))
-    document.getElementsByClassName("show")[0].classList.remove("show");
-    document.getElementById("cave").classList.add("show");
-  })
-  document.getElementById("gate-thumb").addEventListener('click', function () {
-    console.log(document.getElementsByClassName("show"))
-    document.getElementsByClassName("show")[0].classList.remove("show");
-    document.getElementById("gate").classList.add("show");
-  })
-  document.getElementById("pals2-thumb").addEventListener('click', function () {
-    console.log(document.getElementsByClassName("show"))
-    document.getElementsByClassName("show")[0].classList.remove("show");
-    document.getElementById("pals2").classList.add("show");
-  })
-  document.getElementById("shower-thumb").addEventListener('click', function () {
-    console.log(document.getElementsByClassName("show"))
-    document.getElementsByClassName("show")[0].classList.remove("show");
-    document.getElementById("shower").classList.add("show");
-  })
-  document.getElementById("deer-thumb").addEventListener('click', function () {
-    console.log(document.getElementsByClassName("show"))
-    document.getElementsByClassName("show")[0].classList.remove("show");
-    document.getElementById("deer").classList.add("show");
-  })
-  document.getElementById("pose-thumb").addEventListener('click', function () {
-    console.log(document.getElementsByClassName("show"))
-    document.getElementsByClassName("show")[0].classList.remove("show");
-    document.getElementById("pose").classList.add("show");
-  })
-  document.getElementById("memorial-thumb").addEventListener('click', function () {
-    console.log(document.getElementsByClassName("show"))
-    document.getElementsByClassName("show")[0].classList.remove("show");
-    document.getElementById("memorial").classList.add("show");
-  })
+  //
+  // pics[0].style.opacity = "1.0";
+  // pics[2].style.opacity = "0.0";
+  //
+  // window.setTimeout( function () {console.log("chico is hungry");}, 20000);
+  //
+  // pics[0].style.opacity = "0.0";
+  // pics[1].style.opacity = "1.0";
+
 }
+
+function changePicture(pics, j, i) {
+  console.log(j, i)
+  pics[j].style.opacity = "0.0";
+  pics[i].style.opacity = "1.0";
+}
+
+
+//   document.getElementById("pals-thumb").addEventListener('click', function () {
+//     console.log(document.getElementsByClassName("show"))
+//     document.getElementsByClassName("show")[0].classList.remove("show");
+//     document.getElementById("pals").classList.add("show");
+//   })
+//   document.getElementById("lex-thumb").addEventListener('click', function () {
+//     console.log(document.getElementsByClassName("show"))
+//     document.getElementsByClassName("show")[0].classList.remove("show");
+//     document.getElementById("lex").classList.add("show");
+//   })
+//   document.getElementById("cave-thumb").addEventListener('click', function () {
+//     console.log(document.getElementsByClassName("show"))
+//     document.getElementsByClassName("show")[0].classList.remove("show");
+//     document.getElementById("cave").classList.add("show");
+//   })
+//   document.getElementById("gate-thumb").addEventListener('click', function () {
+//     console.log(document.getElementsByClassName("show"))
+//     document.getElementsByClassName("show")[0].classList.remove("show");
+//     document.getElementById("gate").classList.add("show");
+//   })
+//   document.getElementById("pals2-thumb").addEventListener('click', function () {
+//     console.log(document.getElementsByClassName("show"))
+//     document.getElementsByClassName("show")[0].classList.remove("show");
+//     document.getElementById("pals2").classList.add("show");
+//   })
+//   document.getElementById("shower-thumb").addEventListener('click', function () {
+//     console.log(document.getElementsByClassName("show"))
+//     document.getElementsByClassName("show")[0].classList.remove("show");
+//     document.getElementById("shower").classList.add("show");
+//   })
+//   document.getElementById("deer-thumb").addEventListener('click', function () {
+//     console.log(document.getElementsByClassName("show"))
+//     document.getElementsByClassName("show")[0].classList.remove("show");
+//     document.getElementById("deer").classList.add("show");
+//   })
+//   document.getElementById("pose-thumb").addEventListener('click', function () {
+//     console.log(document.getElementsByClassName("show"))
+//     document.getElementsByClassName("show")[0].classList.remove("show");
+//     document.getElementById("pose").classList.add("show");
+//   })
+//   document.getElementById("memorial-thumb").addEventListener('click', function () {
+//     console.log(document.getElementsByClassName("show"))
+//     document.getElementsByClassName("show")[0].classList.remove("show");
+//     document.getElementById("memorial").classList.add("show");
+//   })
+// }
