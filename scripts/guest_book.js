f3dd1aa6b4d397caa5c922b82057c757aa9a1039
George Pence <penceg@oregonstate.edu>
2021-05-17 16:32:43 -0400

    4:30 pm Monday Commit

--- scripts/guest_book.js
+++ scripts/guest_book.js
@@ -1,9 +1,10 @@
+const fs = require('fs');
 
 document.addEventListener('DOMContentLoaded', startListening);
 
 function startListening() {
   document.getElementById("message").addEventListener("click", openForm);
-  document.getElementById("submit").addEventListener("click", sendMessage);
+  // document.getElementById("submit").addEventListener("click", sendIt);
 }
 
 // The page initially opens to show the guest book entries that have been made,
@@ -17,6 +18,17 @@ function openForm() {
   }
 }
 
+function sendIt() {
+
+  fs.readFile('/nfs/stak/users/penceg/public_html/roscoe-memorial/guest_book.txt', 'utf8', (err, data) => {
+    if(err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  })
+}
+
 // Sends memorial message to API, receive back new display info
 function sendMessage() {
 
@@ -27,19 +39,30 @@ function sendMessage() {
   to_send.email = document.getElementById("e-mail").value;
   to_send.anonymous = document.getElementById("hide-name").checked;
   
+  console.log(to_send)
   // Construct XMLHttp request
-  post_req = new XMLHttpRequest();
-  send_this = JSON.stringify(to_send);
+  let post_req = new XMLHttpRequest();
+  let send_this = JSON.stringify(to_send);
+  http://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php
+  // post_req.open('POST', 'http://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php', true);
   post_req.open('POST', 'http://flip3.engr.oregonstate.edu:7210', true);
   post_req.setRequestHeader('Content-Type', 'application/json');
   console.log("I'm here now!")
   
   // Asynchronous handling
-  // post_req.addEventListener('load', function() {
- 
-    // Display response
+  post_req.addEventListener('load', function() {
+    if (post_req.status >= 200 && post_req.status < 400) {
+      console.log("No Error")
+      // Display response
+      let post_response = JSON.parse(post_req.responseText).json;
+      let display_messages = document.getElementById("guest-book-messages");
+      display_messages.innerHTML = post_response();
     
-  // }
+    } else {
+      console.log("Error in network request (guest_book): " + post_req.statusText);
+    }
+  });
   
+  post_req.send(send_this);
   event.preventDefault();
 }
