89d7fc9ff371f567e121a236bb63eaf2528339a0
George Pence <penceg@oregonstate.edu>
2021-05-22 07:15:21 -0400

    May 22 7:15 am commit

--- scripts/guest_book.js
+++ scripts/guest_book.js
@@ -2,93 +2,38 @@ document.addEventListener('DOMContentLoaded', startListening);
 
 function startListening() {
   document.getElementById("message").addEventListener("click", openForm);
-  document.getElementById("submit").addEventListener("click", sendIt);
+  document.getElementById("submit").addEventListener("click", sendMessage);
 }
 
 // The page initially opens to show the guest book entries that have been made,
 // with just one 'Leave a message' field visible.  When the user clicks on that
 // field, several other fields become visible for user entry.
+//
+// The website * should * store previous comments and display them, but after
+// wrestling with the engineering server security for two weeks, I gave up on
+// that.  I did hard code (HTML) one 'previous' comment.
+//
 function openForm() {
   let items = document.getElementsByClassName("hidden");
   let num_items = items.length
   for (let i = 0; i <= num_items - 1; i += 1) {
+    items[0].classList.add("show-me");
     items[0].classList.remove("hidden");
   }
 }
 
-function sendIt() {
-  
-  // Get form data organized.
-  let to_send = {};
-  let today = new Date();
-  
-  to_send.date = (today.getMonth()+1) + '/' +today.getDate() + '/' + today.getFullYear();
-  to_send.message = document.getElementById("message").value;
-  to_send.name = document.getElementById("name").value;
-  to_send.email = document.getElementById("e-mail").value;
-  to_send.anonymous = document.getElementById("hide-name").checked;
-  
-  // // Construct XMLHttp request - only purpose is to satisfy form send requirement
-  // let post_req = new XMLHttpRequest();
-  // let send_this = JSON.stringify(to_send);
-  //
-  // // post_req.open('POST', 'https://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php', true);
-  // post_req.open('POST', 'https://httpbin.org/post', true);
-  // post_req.setRequestHeader('Content-Type', 'application/json');
-  //
-  // // Asynchronous handling
-  // post_req.addEventListener('load', function() {
-  //   if (post_req.status >= 200 && post_req.status < 400) {
-  //
-  //     // Get response
-  //     let post_response = JSON.parse(post_req.responseText).json;
-  //
-  //     // Update database of Messages (guest book signings)
-  //     document.getElementById("guest-book-messages").innerHTML = post_response.message
-  //     // updateGuestBook(post_response);
-  //
-  //     // Display messages, with new message added at the top
-  //     // displayGuestBook();
-  //
-  //   } else {
-  //     console.log("Error in network request (httpbin): " + post_req.statusText);
-  //   }
-    
-    
-    //  ========================================================================
-    
-    //  ========================================================================
-    // Construct HTTP request to get previous messages from file
-    let date = "date=" + to_send.date;
-    let mess = "message=" + to_send.message;
-    let name = "name=" + to_send.name;
-    let email = "email=" + to_send.email;
-    let anon = "anonymous=" + to_send.anonymous;
-    let send_to_url = "http://flip3.engr.oregonstate.edu:7210"
-    let send_url = send_to_url + "?" + date + "&" + mess + "&"+ name + "&"+ email + "&" + anon
-    
-    let get_req = new XMLHttpRequest()
-    // post_2.open('GET', 'https://web.engr.oregonstate.edu/~penceg/roscoe-guestbook/', true);
-    get_req.open('GET', send_url, true);
-    
-    get_req.addEventListener('load', function() {
-      if (get_req.status >= 200 && get_req.status < 400) {
-        let get_response = JSON.parse(get_req.responseText)
-        document.getElementById("guest-book-messages").innerHTML += get_response
-      }else {
-        console.log("Error in network request (guest_book): " + post_req.statusText);
-      }
-  
-    });
-
-    //  ========================================================================
-  get_req.send(null);
-  event.preventDefault()
+function closeForm() {
+  document.getElementById("message").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("e-mail").value = "";
+  document.getElementById("hide-name").checked = false;
+  let items = document.getElementsByClassName("show-me");
+  let num_items = items.length
+  for (let i = 0; i <= num_items - 1; i += 1) {
+    items[0].classList.add("hidden");
+    items[0].classList.remove("show-me");
+  }
 }
-  
-//   post_req.send(send_this);
-//   event.preventDefault();
-// }
 
 // Sends memorial message to API, receive back new display info
 function sendMessage() {
@@ -103,11 +48,11 @@ function sendMessage() {
   to_send.email = document.getElementById("e-mail").value;
   to_send.anonymous = document.getElementById("hide-name").checked;
   
-  // Construct XMLHttp request - only purpose is to satisfy form send requirement
+  // Construct XMLHttp request - the only purpose here is to satisfy form send
+  // requirement.
   let post_req = new XMLHttpRequest();
   let send_this = JSON.stringify(to_send);
   
-  // post_req.open('POST', 'https://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php', true);
   post_req.open('POST', 'https://httpbin.org/post', true);
   post_req.setRequestHeader('Content-Type', 'application/json');
   
@@ -117,45 +62,18 @@ function sendMessage() {
       
       // Get response
       let post_response = JSON.parse(post_req.responseText).json;
+      console.log(post_response);
       
-      // Update database of Messages (guest book signings)
-      document.getElementById("guest-book-messages").innerHTML = post_response.message
-      // updateGuestBook(post_response);
+      // Close form, clear fields, and display new message
+      closeForm()
+      document.getElementById("guest-book-message").innerHTML = post_response.message;
+      let signature = post_response.date + "     " +
+          (to_send.anonymous ? "" : post_response.name);
+      document.getElementById("guest-book-date-name").innerHTML = signature
       
-      // Display messages, with new message added at the top
-      // displayGuestBook();
-    
     } else {
       console.log("Error in network request (httpbin): " + post_req.statusText);
     }
-    
-  
-    //  ========================================================================
-    
-    //  ========================================================================
-    // Construct HTTP request to get previous messages from file
-    let date = "date=" + to_send.date;
-    let mess = "message=" + to_send.message;
-    let name = "name=" + to_send.name;
-    let email = "email=" + to_send.email;
-    let anon = "anonymous=" + to_send.anonymous;
-    let send_to_url = "http://flip3.engr.oregonstate.edu:7210"
-    let send_url = send_to_url + "?" + date + "&" + mess + "&"+ name + "&"+ email + "&" + anon
-  
-    let get_req = new XMLHttpRequest()
-    // post_2.open('GET', 'https://web.engr.oregonstate.edu/~penceg/roscoe-guestbook/', true);
-    get_req.open('GET', send_url, true);
-
-    get_req.addEventListener('load', function() {
-      if (get_req.status >= 200 && get_req.status < 400) {
-        let get_response = JSON.parse(get_req.responseText)
-        document.getElementById("guest-book-messages").innerHTML += get_response
-      }else {
-        console.log("Error in network request (guest_book): " + post_req.statusText);
-      }
-    });
-    get_req.send(null);
-    //  ========================================================================
     
   });
   
